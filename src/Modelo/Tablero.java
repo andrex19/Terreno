288bc14e5d347475b7b9228b96bfe9af7343386b
Andrés Muñoz <af.munoz19@gmail.com>
2016-06-08 23:26:36 -0300

    arreglando a mvc 1.0

---
+++ src/Modelo/Tablero.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Andres Muñoz Bravo
+ */
+public class Tablero {
+    public Casilla[][] infoCasillas=new Casilla[15][15];
+    public void Tablero(){
+        for (int i=0;i<15;i++){
+            for (int j=0;j<15;j++){
+                this.infoCasillas[i][j]=new Casilla();
+            }
+       
+        
+        }
+        
+    }
+    
+}
